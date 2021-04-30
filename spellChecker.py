2b1a4f4125197e6e3679d0780e712b965e52da82
Shahriyar Mammadli <smammadli@pasha-holding.ph>
2021-04-30 17:05:02 +0400

    Word list is formed

--- spellChecker.py
+++ spellChecker.py
@@ -1,57 +1,74 @@
 # Shahriyar Mammadli
+# The code performs statistical (not AI based) corrections of spelling errors or typos.
 # Import required libraries
 import re
 from collections import Counter
 import pickle
 
 # Set parameters
-# Read the alphabet
-with open('alphabet.txt', encoding="utf8") as f:
-    alphabet = f.readline()
+# Set reference text file path
+fileName = 'referenceText.txt'
+# Set the alphabet path
+alphabetPath = 'alphabet.txt'
 
-def words(text):
-    return re.findall(r'\w+', text.lower())
+# This function reads a text file and creates a table of words and their frequencies
+def formDictionary(fileName):
+    # Create the frequency table of the words
+    WORDS = Counter(re.findall(r'\w+', open(fileName, encoding="utf8").read().lower()))
+    # Write the result into a pickle file
+    with open('container.pickle', 'wb') as handle:
+        pickle.dump(WORDS, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
-WORDS = Counter(words(open('referenceText.txt', encoding="utf8").read()))
-
-with open('container.pickle', 'wb') as handle:
-    pickle.dump(WORDS, handle, protocol=pickle.HIGHEST_PROTOCOL)
-
-with open('container.pickle', 'rb') as handle:
-    WORDS = pickle.load(handle)
-
-def P(word, N=sum(WORDS.values())):
-    "Probability of `word`."
-    return WORDS[word] / N
-
-def correction(word, alphabet):
-    "Most probable spelling correction for word."
-    return max(candidates(word, alphabet), key=P)
-
+# Returns the candidate words for the input string
+# Possibilities are 1. input word is already a known word, no need...
+# ...for correction, 2. input word is one edit away from the original word,
+# ...3. input word is two edit away from the original word, 4. could not succeed...
+# ...to find a candidate word and returns the input word itself.
 def candidates(word, alphabet):
-    "Generate possible spelling corrections for word."
-    return (known([word]) or known(edits1(word, alphabet)) or known(edits2(word, alphabet)) or [word])
+    return (known([word]) or known(distance1(word, alphabet)) or known(distance2(word, alphabet)) or [word])
 
+# Filter out the words that are not in the dictionary
 def known(words):
-    "The subset of `words` that appear in the dictionary of WORDS."
     return set(w for w in words if w in WORDS)
 
-def edits1(word, alphabet):
-    "All edits that are one edit away from `word`."
-    splits = [(word[:i], word[i:])    for i in range(len(word) + 1)]
-    deletes = [L + R[1:]               for L, R in splits if R]
+# Do the spelling fixing operations in one distance
+def distance1(word, alphabet):
+    splits = [(word[:i], word[i:]) for i in range(len(word) + 1)]
+    deletes = [L + R[1:] for L, R in splits if R]
     transposes = [L + R[1] + R[0] + R[2:] for L, R in splits if len(R)>1]
-    replaces = [L + c + R[1:]           for L, R in splits if R for c in alphabet]
-    inserts = [L + c + R               for L, R in splits for c in alphabet]
+    replaces = [L + c + R[1:] for L, R in splits if R for c in alphabet]
+    inserts = [L + c + R for L, R in splits for c in alphabet]
     return set(deletes + transposes + replaces + inserts)
 
-def edits2(word, alphabet):
-    "All edits that are two edits away from `word`."
-    return (e2 for e1 in edits1(word, alphabet) for e2 in edits1(e1, alphabet))
+# Do the spelling fixing operations in two distance
+def distance2(word, alphabet):
+    return (e2 for e1 in distance1(word, alphabet) for e2 in distance1(e1, alphabet))
 
 # Correct words of a sentence
 def correctSentence(sentence, alphabet):
-    corrections = [correction(word, alphabet) for word in sentence.split()]
+    corrections = [max(candidates(word, alphabet), key=P) for word in sentence.split()]
     return (' '.join(corrections))
 
-print(correctSentence('Dünizə gedun yorda ümami yaxınaşma', alphabet))
+try:
+    print("Trying to read pre-formed word-frequency table.")
+    with open('container.pickle', 'rb') as handle:
+        WORDS = pickle.load(handle)
+except:
+    print("Couldn't find the table, trying to recreate it using the reference text.")
+    try:
+        formDictionary(fileName)
+        with open('container.pickle', 'rb') as handle:
+            WORDS = pickle.load(handle)
+    except:
+        print("Couldn't find the reference file.")
+        quit()
+
+# Probablity calculation of the word is calculated basing on the frequency of the word...
+# ...in the reference text.
+def P(word, N=sum(WORDS.values())):
+    return WORDS[word] / N
+
+# Read the alphabet
+with open(alphabetPath, encoding="utf8") as f:
+    alphabet = f.readline()
+print(correctSentence('Dünizin səthinnda üfüqq xət', alphabet))
---
+++ spellCheckerSymSpell.py
@@ -0,0 +1,3 @@
+# Shahriyar Mammadli
+# The code performs smart spell checking using the SymSpell library
+# Import required libraries
